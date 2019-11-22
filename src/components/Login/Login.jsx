cdc67590963593f92a92023622c3fed4e82a94a1
domocliff <kuzaev.ruslan-k4@yandex.ru>
2019-11-22 07:09:54 +0500

    Initial commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Field, reduxForm} from "redux-form";
+import { Input } from '../common/FormsControls/FormsControls';
+import { required } from '../../utils/validators/validators';
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} >
+            <div>
+                <Field placeholder={"Email"} name={"email"} 
+                validate={[required]}
+                component={Input} />
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} 
+                validate={[required]}
+                component={Input} />
+            </div>
+            <div>
+                <Field component={Input} name={"rememberMe"} type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm ({form: 'login'})(LoginForm)
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+}
+
+export default connect(null, {login} )(Login); 

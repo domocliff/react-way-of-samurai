6035b4a7f750bd3a24dbe4c313a919fdb83030ef
domocliff <kuzaev.ruslan-k4@yandex.ru>
2019-11-25 14:53:15 +0500

    api key change

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "3ac62e9e-abd3-4962-8850-16b4d147a3a3"
     }
 });
 

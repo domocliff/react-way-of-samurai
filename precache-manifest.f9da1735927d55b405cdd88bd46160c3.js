bda166b33e2bbb6482570ab2f527e73b0134997f
domocliff <kuzaev.ruslan-k4@yandex.ru>
2019-11-25 15:44:03 +0500

    Updates

---
+++ precache-manifest.f9da1735927d55b405cdd88bd46160c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d503957ca90fdb72b60353e690c7585",
+    "url": "/react-way-of-samurai/index.html"
+  },
+  {
+    "revision": "c6ee52e9980a45160640",
+    "url": "/react-way-of-samurai/static/css/3.8f5ec095.chunk.css"
+  },
+  {
+    "revision": "28e1519e6cb605222cc6",
+    "url": "/react-way-of-samurai/static/css/4.be065bdd.chunk.css"
+  },
+  {
+    "revision": "58769dc15512998cee6f",
+    "url": "/react-way-of-samurai/static/css/main.53d27f51.chunk.css"
+  },
+  {
+    "revision": "171893f01b63cfe07059",
+    "url": "/react-way-of-samurai/static/js/2.0a1c9317.chunk.js"
+  },
+  {
+    "revision": "c6ee52e9980a45160640",
+    "url": "/react-way-of-samurai/static/js/3.11432d87.chunk.js"
+  },
+  {
+    "revision": "28e1519e6cb605222cc6",
+    "url": "/react-way-of-samurai/static/js/4.b6f3ff91.chunk.js"
+  },
+  {
+    "revision": "58769dc15512998cee6f",
+    "url": "/react-way-of-samurai/static/js/main.669fb13c.chunk.js"
+  },
+  {
+    "revision": "506ed5889697d56803c1",
+    "url": "/react-way-of-samurai/static/js/runtime-main.024be42b.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/react-way-of-samurai/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/react-way-of-samurai/static/media/user.8b1dbb1f.png"
+  }
+]);
